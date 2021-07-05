3831e9cb79eaf90bfeb8860fe9acdd493fbad885
biyam <73857334+biyam@users.noreply.github.com>
2021-07-05 14:58:53 +0900

    Update 게임 개발.py

--- 구현/part2/게임 개발.py
+++ 구현/part2/게임 개발.py
@@ -1,37 +1,36 @@
-# N, M을 공백을 기준으로 구분하여 입력받기
 n, m = map(int, input().split())
 
-# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+# 방문했던 위치를 넣을 d 생성, 0으로 초기화
 d = [[0] * m for _ in range(n)]
-# 현재 캐릭터의 X 좌표, Y 좌표, 방향을 입력받기
 x, y, direction = map(int, input().split())
 d[x][y] = 1 # 현재 좌표 방문 처리
 
-# 전체 맵 정보를 입력받기
+# 현재 맵을 넣:을 array 생성
 array = []
-for i in range(n):
-    array.append(list(map(int, input().split())))
+for i in range(n)
+    array.append(list(map(int,input().split())))
 
-# 북, 동, 남, 서 방향 정의
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+
+# 북, 동, 남, 서 방향 정의 
+# 0, 1, 2, 3
+dx = [-1, 0, 1, 0] # 세로
+dy = [0, 1, 0, -1] # 가로
 
 # 왼쪽으로 회전
 def turn_left():
     global direction
     direction -= 1
+    # 0이 끝이므로 -1이 되면 한 바퀴 돌리기
     if direction == -1:
         direction = 3
 
-# 시뮬레이션 시작
 count = 1
 turn_time = 0
 while True:
-    # 왼쪽으로 회전
     turn_left()
     nx = x + dx[direction]
     ny = y + dy[direction]
-    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    # 기존과 정면 모두 0일 때 count를 늘린다.
     if d[nx][ny] == 0 and array[nx][ny] == 0:
         d[nx][ny] = 1
         x = nx
@@ -39,21 +38,21 @@ while True:
         count += 1
         turn_time = 0
         continue
-    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    # 회전 후 정면이 1일 때
     else:
+        # 한 번 더 돌린다
         turn_time += 1
-    # 네 방향 모두 갈 수 없는 경우
+    # 4번 돌려본 결과 상하좌우 모두 갈 수 없을 때 
     if turn_time == 4:
         nx = x - dx[direction]
         ny = y - dy[direction]
-        # 뒤로 갈 수 있다면 이동하기
+        # 뒤로 갈 수 있으면 이동해서 다시 시작
         if array[nx][ny] == 0:
             x = nx
             y = ny
-        # 뒤가 바다로 막혀있는 경우
+        # 뒤가 바다면 끝내기
         else:
             break
         turn_time = 0
 
-# 정답 출력
 print(count)
